46c9a8b0966174ad7ba945da7cab273537e91fc4
RB <rinobroendum@gmail.com>
2024-10-17 13:23:46 +0200

    repospage output

--- app/code/repos/page.jsx
+++ app/code/repos/page.jsx
@@ -1,3 +1,8 @@
+import Link from "next/link";
+import {FaStar, FaCodeBranch, FaEye} from "react-icons/fa";
+
+
+
 async function fetchRepos() {
     const response = await fetch(
       "https://api.github.com/users/bradtraversy/repos"
@@ -11,7 +16,32 @@ const ReposPage = async () => {
     const repos = await fetchRepos();
   
 
-  return <div>{repos[0].name}</div>;
+  return (
+    <div className="repos-container">
+      <h2>Repositories</h2>
+      <ul className="repo-list" >
+    
+        {repos.map((repo) => (
+          <li key={repo.id}>
+            <Link href={`/code/repos/${repo.name}`}>
+              <h3>{repo.name}</h3>
+              <p>{repo.description}</p>
+              <div className="repo-details"></div>
+              <span>
+                <FaStar /> {repo.stargazers_count}
+              </span>
+              <span>
+                <FaCodeBranch /> {repo.forks_count} 
+              </span>
+              <span>
+                <FaEye /> {repo.watchers_count}
+              </span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 
 };
 
