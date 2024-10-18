e26d569267cdb6a0f24b496df5753a96cc22d4ac
RB <rinobroendum@gmail.com>
2024-10-18 12:31:11 +0200

    repo & repodir component, suspense boundaries

--- app/code/repos/[name]/page.jsx
+++ app/code/repos/[name]/page.jsx
@@ -1,11 +1,21 @@
-import Repo from '@/app/components/Repo';
+import { Suspense } from "react";
+import Repo from "@/app/components/Repo";
+import RepoDirs from "@/app/components/RepoDirs";
+import Link from "next/link";
 
-
-const RepoPage = ({params: {name}}) => {
-  return ( 
-  <div className="card">
-    < Repo name={name} />
-  </div>
+const RepoPage = ({ params: { name } }) => {
+  return (
+    <div className="card">
+      <Link href="/code/repos" className="btn btn-back">
+        Back to Repositories
+      </Link>
+      <Suspense fallback={<div>Loading repo...</div>}>
+      <Repo name={name} />
+      </Suspense>
+      <Suspense fallback={<div>Loading directories...</div>}>
+      <RepoDirs name={name} />
+      </Suspense>
+    </div>
   );
 };
 
